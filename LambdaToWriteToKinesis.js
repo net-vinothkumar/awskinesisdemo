5ba8afdd591c1e2b3c3318b30da952659faeadea
net-vinothkumar <30971809+net-vinothkumar@users.noreply.github.com>
2019-09-18 21:52:17 +0200

    Update LambdaToWriteToKinesis.js

--- LambdaToWriteToKinesis.js
+++ LambdaToWriteToKinesis.js
@@ -1,1 +1,32 @@
 
+var AWS = require('aws-sdk');
+
+exports.handler = function(event, context, callback) {
+
+   var kinesis = new AWS.Kinesis();
+
+kinesis.putRecord({
+
+  Data: '{"action": "click", "productId": "lunchbag"}',
+
+  PartitionKey: 'user-123',
+
+  StreamName: 'testkinesis'
+
+}, function(err, data) {
+
+  if (err) {
+
+    console.log(err, err.stack); // an error occurred
+
+  } else {
+
+    console.log(data); // successful response
+
+    return data;
+
+  }
+
+});
+
+}
