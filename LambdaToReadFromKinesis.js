c189992c3ea11d91d267483337dd889ecce25f96
net-vinothkumar <30971809+net-vinothkumar@users.noreply.github.com>
2019-09-18 21:53:06 +0200

    Update LambdaToReadFromKinesis.js

--- LambdaToReadFromKinesis.js
+++ LambdaToReadFromKinesis.js
@@ -1,1 +1,76 @@
 
+var AWS = require('aws-sdk');
+
+exports.handler = function(event, context, callback) {
+
+   var kinesis = new AWS.Kinesis();
+
+kinesis.describeStream({
+
+  StreamName: 'testkinesis'
+
+}, function(err, streamData) {
+
+  if (err) {
+
+    console.log(err, err.stack); // an error occurred
+
+  } else {
+
+    console.log(streamData); // successful response
+
+    streamData.StreamDescription.Shards.forEach(shard => {
+
+      kinesis.getShardIterator({
+
+        ShardId: shard.ShardId,
+
+        ShardIteratorType: 'TRIM_HORIZON',
+
+        StreamName: 'testkinesis'
+
+      }, function(err, shardIteratordata) {
+
+        if (err) {
+
+          console.log(err, err.stack); // an error occurred
+
+        } else {
+
+          console.log(shardIteratordata); // successful response
+
+          kinesis.getRecords({
+
+            ShardIterator: shardIteratordata.ShardIterator
+
+          }, function(err, recordsData) {
+
+            if (err) {
+
+              console.log(err, err.stack); // an error occurred
+
+            } else {
+
+             
+
+              recordsData.Records.forEach( record => {
+
+                                console.log( record.Data.toString(), shard.ShardId );
+
+                            } );
+
+            }
+
+          });
+
+        }
+
+      });
+
+    });
+
+  }
+
+});
+
+};
